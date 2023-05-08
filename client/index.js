91aa87fcf179c8ad0f8dcdf3002fe86335d90b55
angyts <punggolzenith@gmail.com>
2023-05-08 22:54:45 +0800

    completed MVP

--- client/index.js
+++ client/index.js
@@ -6,9 +6,20 @@ const serverUrl = 'http://localhost:1225';
 
 async function main() {
   // TODO: how do we prove to the server we're on the nice list? 
+  const args = process.argv;
+  // console.log(args);
+  if (args.length !== 3){
+    throw new Error('Usage: node index "the name you want to test"')
+  }
+
+  const name = args[2];
+  const index = niceList.findIndex(n => n === name);
+  const merkleTree = new MerkleTree(niceList);
+  const proof = merkleTree.getProof(index);
 
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
+    name: name,
+    proof: proof
   });
 
   console.log({ gift });
